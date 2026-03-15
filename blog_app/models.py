e3408681cc905f7c18c14799dfb437238b61c4eb
mtaruntejeswar <taruntejeswar.marugundla@gmail.com>
2026-03-15 23:34:07 +0530

    login_view completed

--- blog_app/models.py
+++ blog_app/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     bio = models.CharField(max_length=200)
-    profile_pic = models.ImageField(upload_to='profile_pics/')
+    profile_pic = models.ImageField(upload_to='profile_pics/',blank=True,default='profile_pics/default_profile_pic.svg')
+    
+    def __str__(self):
+        return self.user.username
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .models import Profile
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 
 def home(request):
-    return render(request,'blog_app/home.html')
+    profile = Profile.objects.get(user=request.user)
+    context = {
+        'profile':profile
+    }
+    return render(request,'blog_app/home.html',context)
 
 
 
 def login_view(request):
     if request.method == 'POST':
-        email = request.POST.get('email')
+        username = request.POST.get('username')
         password = request.POST.get('password')
+        
+        user = authenticate(request,username=username,password=password)
+        
+        if user is None:
+            print("Invalid Credentials!")
+            return redirect('login')
+        else:
+            login(request,user)
+            return redirect('home')
         
         
     return render(request,'blog_app/login.html')
+
+
